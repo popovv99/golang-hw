3f38dbb91695f7b1703bed61c3da3921ee30d60a
vpopov <vpopov@diasoft.ru>
2026-04-30 11:33:45 +0700

    hw08_envdir_tool

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 	// Place your code here.
+	if len(os.Args) < 3 {
+		fmt.Println("usage: go-envdir <env-dir> <command> [args...]")
+		return
+	}
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	returnCode := RunCmd(os.Args[2:], env)
+	os.Exit(returnCode)
 }
